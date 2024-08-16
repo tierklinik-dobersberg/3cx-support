e755dfec80c48cf4ece8b07a36f97494367dbae6
Patrick Pacher <patrick.pacher@gmail.com>
2024-08-16 15:32:28 +0200

    Add more logging

--- internal/database/customer_resolver.go
+++ internal/database/customer_resolver.go
@@ -2,10 +2,10 @@ package database
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"sync"
-	"sync/atomic"
-	"time"
 
 	"github.com/hashicorp/go-multierror"
 	"github.com/sirupsen/logrus"
@@ -41,154 +41,129 @@ func NewCustomerResolver(db Database, cli customerv1connect.CustomerServiceClien
 
 func (cr *CustomerResolver) Query(ctx context.Context, query *SearchQuery) ([]*pbx3cxv1.CallEntry, []*customerv1.Customer, error) {
 
-	var wg sync.WaitGroup
-	var pending atomic.Int64
-
 	errs := new(multierror.Error)
 
 	// this one cancels as soon as the h2 stream ends
-	resultChan, errChan := cr.db.StreamSearch(ctx, query)
-
-	stream := cr.cli.SearchCustomerStream(ctx)
 
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
+	resultChan, errChan := cr.db.StreamSearch(ctx, query)
+	stream := cr.cli.SearchCustomerStream(ctx)
+
 	go func() {
-		defer log.L(ctx).Infof("receive loop finished")
-
+	L:
 		for {
-			msg, err := stream.Receive()
-			if err != nil {
-				log.L(ctx).Errorf("failed to receive message: %s", err)
-
-				return
-			}
-
-			pending.Add(-1)
-
-			// for each sent request, we add 1 to the waitgroup
-			wg.Done()
-
-			cr.customerLock.Lock()
-			for _, c := range msg.Results {
-				cr.customers[c.Customer.Id] = c.Customer
-			}
-			cr.customerLock.Unlock()
-
-			for _, c := range msg.Results {
-				log.L(ctx).Infof("received customer response %s %s (%s)", c.Customer.FirstName, c.Customer.LastName, c.Customer.Id)
-			}
-		}
-	}()
-
-L:
-	for {
-		select {
-		case <-ctx.Done():
-			log.L(ctx).Infof("request context cancelled")
-
-			break L
-
-		case record, ok := <-resultChan:
-			if !ok {
-				log.L(ctx).Infof("result channel closed")
+			select {
+			case <-ctx.Done():
+				log.L(ctx).Infof("request context cancelled")
 
 				break L
-			}
 
-			// first, append the record to the result list
-			cr.recordsLock.Lock()
-			cr.records = append(cr.records, record)
-			cr.recordsLock.Unlock()
-
-			// next, check if we need to fetch a customer record
-			if record.CustomerID != "" {
-				cr.customerLock.Lock()
-				_, ok := cr.customers[record.CustomerID]
-				cr.customerLock.Unlock()
-
+			case record, ok := <-resultChan:
 				if !ok {
-					// search for the customer
-					_, err, _ := cr.inflight.Do(record.CustomerID, func() (interface{}, error) {
-						cr.customerLock.Lock()
-						cr.customers[record.CustomerID] = nil
-						cr.customerLock.Unlock()
+					log.L(ctx).Infof("result channel closed")
 
-						logrus.Infof("sending customer query for %s/%s", record.CustomerSource, record.CustomerID)
-						wg.Add(1)
-						pending.Add(1)
+					break L
+				}
 
-						if record.CustomerSource == "" {
-							if err := stream.Send(&customerv1.SearchCustomerRequest{
-								Queries: []*customerv1.CustomerQuery{
-									{
-										Query: &customerv1.CustomerQuery_Id{
-											Id: record.CustomerID,
-										},
-									},
-								},
-							}); err != nil {
-								wg.Done()
-								pending.Add(-1)
+				// first, append the record to the result list
+				cr.recordsLock.Lock()
+				cr.records = append(cr.records, record)
+				cr.recordsLock.Unlock()
 
-								return nil, fmt.Errorf("failed to send query: %w", err)
-							}
-						} else {
-							if err := stream.Send(&customerv1.SearchCustomerRequest{
-								Queries: []*customerv1.CustomerQuery{
-									{
-										Query: &customerv1.CustomerQuery_InternalReference{
-											InternalReference: &customerv1.InternalReferenceQuery{
-												Importer: record.CustomerSource,
-												Ref:      record.CustomerID,
+				// next, check if we need to fetch a customer record
+				if record.CustomerID != "" {
+					cr.customerLock.Lock()
+					_, ok := cr.customers[record.CustomerID]
+					cr.customerLock.Unlock()
+
+					if !ok {
+						// search for the customer
+						_, err, _ := cr.inflight.Do(record.CustomerID, func() (interface{}, error) {
+							cr.customerLock.Lock()
+							cr.customers[record.CustomerID] = nil
+							cr.customerLock.Unlock()
+
+							logrus.Infof("sending customer query for %s/%s", record.CustomerSource, record.CustomerID)
+
+							if record.CustomerSource == "" {
+								if err := stream.Send(&customerv1.SearchCustomerRequest{
+									Queries: []*customerv1.CustomerQuery{
+										{
+											Query: &customerv1.CustomerQuery_Id{
+												Id: record.CustomerID,
 											},
 										},
 									},
-								},
-							}); err != nil {
-								wg.Done()
-								pending.Add(-1)
-
-								return nil, fmt.Errorf("failed to send query: %w", err)
+								}); err != nil {
+									return nil, fmt.Errorf("failed to send query: %w", err)
+								}
+							} else {
+								if err := stream.Send(&customerv1.SearchCustomerRequest{
+									Queries: []*customerv1.CustomerQuery{
+										{
+											Query: &customerv1.CustomerQuery_InternalReference{
+												InternalReference: &customerv1.InternalReferenceQuery{
+													Importer: record.CustomerSource,
+													Ref:      record.CustomerID,
+												},
+											},
+										},
+									},
+								}); err != nil {
+									return nil, fmt.Errorf("failed to send query: %w", err)
+								}
 							}
+
+							return nil, nil
+						})
+						if err != nil {
+							log.L(ctx).Errorf("failed to send customer lookup query: %s", err)
+
+							cancel()
 						}
-
-						return nil, nil
-					})
-					if err != nil {
-						log.L(ctx).Errorf("failed to send customer lookup query: %s", err)
-
-						cancel()
 					}
 				}
+
+			case err, ok := <-errChan:
+				if !ok {
+					log.L(ctx).Infof("error channel closed")
+
+					break L
+				}
+
+				errs.Errors = append(errs.Errors, err)
+			}
+		}
+
+		if err := stream.CloseRequest(); err != nil {
+			log.L(ctx).Errorf("failed to close request side of stream: %s", err)
+		}
+	}()
+
+	for {
+		msg, err := stream.Receive()
+		if err != nil {
+			if !errors.Is(err, io.EOF) {
+				log.L(ctx).Errorf("failed to receive message: %s", err)
 			}
 
-		case err, ok := <-errChan:
-			if !ok {
-				log.L(ctx).Infof("error channel closed")
+			break
+		}
 
-				break L
-			}
+		cr.customerLock.Lock()
+		for _, c := range msg.Results {
+			cr.customers[c.Customer.Id] = c.Customer
+		}
+		cr.customerLock.Unlock()
 
-			errs.Errors = append(errs.Errors, err)
+		for _, c := range msg.Results {
+			log.L(ctx).Infof("received customer response %s %s (%s)", c.Customer.FirstName, c.Customer.LastName, c.Customer.Id)
 		}
 	}
 
-	if err := stream.CloseRequest(); err != nil {
-		log.L(ctx).Errorf("failed to close request side of stream: %s", err)
-	}
-
-	log.L(ctx).Infof("waiting for goroutines to finish, count=%d", pending.Load())
-
-	go func() {
-		time.Sleep(time.Second * 30)
-		cancel()
-	}()
-
-	wg.Wait()
-
-	log.L(ctx).Infof("goroutines finished, count=%d", pending.Load())
+	log.L(ctx).Infof("search stream completed")
 
 	cr.customerLock.Lock()
 	defer cr.customerLock.Unlock()
